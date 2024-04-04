cedeb73d2c88c5deff8e11679219e30ab6ec7674
semra-elcelik <semraelcelik@gmail.com>
2024-04-05 02:29:13 +0300

    challenge 1done ConvertDecimalTobinary done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,33 +1,60 @@
 package org.example;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println(checkForPalindrome("I did, did I?"));
+        System.out.println(checkForPalindrome("Racecar"));
+        System.out.println(checkForPalindrome("hello"));
+        System.out.println(checkForPalindrome("Was it a car or a cat I saw?"));
+
+        HashSet<Integer> datas=new HashSet<>();
+        datas.add(25);
+        datas.add(55);
+        datas.add(17);
+        datas.add(0);
+        System.out.println(convertDecimalToBinary(datas));
 
 
     }
 
     public static boolean checkForPalindrome(String input){
-        String tekKelime=input.replaceAll("[.,!\\\\-_?\\\\s]","").toLowerCase();
+       String kucukHarf=input.replace("I","i").toLowerCase();
+       String tekKelime=kucukHarf.replaceAll("[.,!\\-_?\\s]","");
+
         char[] harfler=tekKelime.toCharArray();
-        //list<Character> charList
         char[] rHarfler = new char[harfler.length];
         for(int i=0; i<harfler.length;i++){
             rHarfler[(harfler.length-1)-i]=harfler[i];
         }
-        if(Arrays.equals(rHarfler, harfler)){
-            System.out.println("Polindrom");
-            return true;
-        }
-         return false;
-
-
+        return Arrays.equals(rHarfler,harfler);
 
     }
+
+    public static Set<String> convertDecimalToBinary(HashSet<Integer> datas){
+        Set<String> binaries=new HashSet<>();
+        for(int data:datas){
+            StringBuilder binary=new StringBuilder();
+
+            if(data==0){
+                binary.append("0");
+            }
+            else{
+                int num =data;
+                while(num>0){
+                    int kalan=num%2;
+                    binary.append(kalan);
+                    num=num/2;
+                }
+            }
+            binary.reverse();
+            binaries.add(binary.toString());
+
+        }
+        return binaries;
+    }
+
 
 }
---
+++ src/main/java/org/example/WorkintechList.java
@@ -0,0 +1,8 @@
+package org.example;
+
+
+import java.util.ArrayList;
+
+public class WorkintechList extends ArrayList {
+
+}
