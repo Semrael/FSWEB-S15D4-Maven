15f95e710c9ede69c3feb910ea08522c08c15317
semra-elcelik <semraelcelik@gmail.com>
2024-04-04 17:08:59 +0300

    challenge 1done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,33 @@
 package org.example;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(checkForPalindrome("I did, did I?"));
+
+
+    }
+
+    public static boolean checkForPalindrome(String input){
+        String tekKelime=input.replaceAll("[.,!\\\\-_?\\\\s]","").toLowerCase();
+        char[] harfler=tekKelime.toCharArray();
+        //list<Character> charList
+        char[] rHarfler = new char[harfler.length];
+        for(int i=0; i<harfler.length;i++){
+            rHarfler[(harfler.length-1)-i]=harfler[i];
+        }
+        if(Arrays.equals(rHarfler, harfler)){
+            System.out.println("Polindrom");
+            return true;
+        }
+         return false;
+
+
+
     }
 
 }
