328cb3754b372b2aafb6aeb1a581eaff8f2b9c35
semra-elcelik <semraelcelik@gmail.com>
2024-04-10 02:12:08 +0300

    isPalindrome WorkintechList Decimal to Binary

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,50 +10,116 @@ public class Main {
         System.out.println(checkForPalindrome("hello"));
         System.out.println(checkForPalindrome("Was it a car or a cat I saw?"));
 
-        HashSet<Integer> datas=new HashSet<>();
-        datas.add(25);
-        datas.add(55);
-        datas.add(17);
-        datas.add(0);
-        System.out.println(convertDecimalToBinary(datas));
+
+        //WorkintechList-------------------
+        System.out.println("------------------------------");
+        WorkintechList wList=new WorkintechList();
+        wList.add(7891);
+        wList.add(90);
+        wList.add(1213);
+        wList.add(8234);
+        wList.add(-123);
+
+        System.out.println(wList);
+        wList.sort();
+        System.out.println(wList);
+        System.out.println(wList.remove(0));
+        System.out.println(wList);
+
+
+       //----------toBinary
+        System.out.println("******************************");
+        System.out.println(convertDecimalToBinary(123));
+
+
+        // HashSet<Integer> datas = new HashSet<>();
+//        datas.add(10);
+//        datas.add(5);
+//        datas.add(20);
+//
+//        Set<String> binaries = convertDecimalToBinary(datas);
+//        System.out.println(binaries);
+
+
 
 
     }
 
+//    public static boolean checkForPalindrome(String input){
+//       String kucukHarf=input.replace("I","i").toLowerCase();
+//       String tekKelime=kucukHarf.replaceAll("[.,!\\-_?\\s]","");
+//
+//        char[] harfler=tekKelime.toCharArray();
+//        char[] rHarfler = new char[harfler.length];
+//        for(int i=0; i<harfler.length;i++){
+//            rHarfler[(harfler.length-1)-i]=harfler[i];
+//        }
+//        return Arrays.equals(rHarfler,harfler);
+//        // return rHarfler.equals(harfler);
+//
+//    }
+    //Emre hocanın çözümü
     public static boolean checkForPalindrome(String input){
-       String kucukHarf=input.replace("I","i").toLowerCase();
-       String tekKelime=kucukHarf.replaceAll("[.,!\\-_?\\s]","");
-
-        char[] harfler=tekKelime.toCharArray();
-        char[] rHarfler = new char[harfler.length];
-        for(int i=0; i<harfler.length;i++){
-            rHarfler[(harfler.length-1)-i]=harfler[i];
+        String  cleanedInput=input.replace("I","i").replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        LinkedList<Character> charList=new LinkedList<>();
+        for(char c:cleanedInput.toCharArray()){
+            charList.add(c);
         }
-        return Arrays.equals(rHarfler,harfler);
-
+        while(charList.size()>1){
+            if(!charList.pollFirst().equals(charList.pollLast())){
+                return false;
+            }
+        }
+        return true;
     }
 
-    public static Set<String> convertDecimalToBinary(HashSet<Integer> datas){
-        Set<String> binaries=new HashSet<>();
-        for(int data:datas){
-            StringBuilder binary=new StringBuilder();
 
-            if(data==0){
-                binary.append("0");
-            }
-            else{
-                int num =data;
-                while(num>0){
-                    int kalan=num%2;
-                    binary.append(kalan);
-                    num=num/2;
-                }
-            }
-            binary.reverse();
-            binaries.add(binary.toString());
 
+
+
+
+
+
+
+
+
+//    public static Set<String> convertDecimalToBinary(HashSet<Integer> datas){
+//        Set<String> binaries=new HashSet<>();
+//        for(Integer data:datas){
+//            StringBuilder binary=new StringBuilder();
+//
+//            if(data==0){
+//                binary.append("0");
+//            }
+//            else{
+//                int num =data;
+//                while(num>0){
+//                    int kalan=num%2;
+//                    binary.append(kalan);
+//                    num=num/2;
+//                }
+//            }
+//            binary.reverse();
+//         binaries.add(binary.toString());
+//
+//        }
+//
+//
+//        return binaries;
+//    }
+    // emre hocanın yöntemi
+    public static String convertDecimalToBinary(int decimal){
+        LinkedList<Integer> binaryList=new LinkedList<>();
+        while(decimal>0){
+            int reminder=decimal%2;
+            binaryList.addFirst(reminder);//addFirst yapmammızdaki amaç listeye ekleme sırasını reverse yapmadan halletmek binary için çünkü ilk kalan son binary rakamı oluyo
+            decimal=decimal/2;
         }
-        return binaries;
+        StringBuilder binaryString=new StringBuilder();
+        for(int digit:binaryList){
+            binaryString.append(digit);
+        }
+        return binaryString.toString();
     }
 
 
--- src/main/java/org/example/WorkintechList.java
+++ src/main/java/org/example/WorkintechList.java
@@ -2,7 +2,25 @@ package org.example;
 
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class WorkintechList extends ArrayList {
 
+    @Override
+    public boolean add(Object o) {
+        if(!this.contains(o)){//Contains gelen objeyi arrayListin içinde aratıyor.
+              return super.add(o);
+        }
+        return false;
+    }
+    public void sort(){
+        Collections.sort(this);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        sort();
+        return super.remove(o);
+    }
 }
